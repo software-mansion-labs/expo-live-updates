433450ad933bbbadf22d6d7c3497a2aa2f32f76c
Kamil Owczarz <kamil.owczarz@swmansion.com>
2025-09-25 15:52:52 +0200

    Fix kotlin formatting and add static checks

--- android/src/main/java/expo/modules/liveupdates/service/ServiceAction.kt
+++ android/src/main/java/expo/modules/liveupdates/service/ServiceAction.kt
@@ -1,14 +1,14 @@
 package expo.modules.liveupdates.service
 
 object ServiceAction {
-    const val updateLiveUpdate = "update-liveupdate"
+  const val updateLiveUpdate = "update-liveupdate"
 }
 
 object ServiceActionExtra {
-    const val title = "title"
-    const val text = "text"
-    const val date = "date"
-    const val imageName = "imageName"
-    const val smallImageName = "smallImageName"
-    const val backgroundColor = "backgroundColor"
+  const val title = "title"
+  const val text = "text"
+  const val date = "date"
+  const val imageName = "imageName"
+  const val smallImageName = "smallImageName"
+  const val backgroundColor = "backgroundColor"
 }
