9fd76855b02671dce380f041df01b02a5e929bde
AnCichocka <annacichocka.poczta@gmail.com>
2025-09-26 12:43:59 +0200

    fix formatting

--- android/src/main/java/expo/modules/liveupdates/service/FirebaseService.kt
+++ android/src/main/java/expo/modules/liveupdates/service/FirebaseService.kt
@@ -16,81 +16,73 @@ import kotlin.String
 
 const val FIREBASE_TAG = "FIREBASE SERVICE"
 const val CHANNEL_ID = "Firebase notifications channel"
-const val CHANNEL_DESCRIPTION = "Channel to handle push notifications for Live Updates form Firebase"
+const val CHANNEL_DESCRIPTION =
+  "Channel to handle push notifications for Live Updates form Firebase"
 
-class FirebaseService: FirebaseMessagingService() {
+class FirebaseService : FirebaseMessagingService() {
 
-    var notificationManager: NotificationManager? = null
+  var notificationManager: NotificationManager? = null
 
-    @RequiresApi(Build.VERSION_CODES.O)
-    override fun onCreate() {
-        val androidNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+  @RequiresApi(Build.VERSION_CODES.O)
+  override fun onCreate() {
+    val androidNotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
-        val channel = NotificationChannel(
-            CHANNEL_ID,
-            CHANNEL_DESCRIPTION,
-            NotificationManager.IMPORTANCE_DEFAULT
-        )
+    val channel =
+      NotificationChannel(CHANNEL_ID, CHANNEL_DESCRIPTION, NotificationManager.IMPORTANCE_DEFAULT)
 
-        androidNotificationManager.createNotificationChannel(channel)
-        notificationManager = androidNotificationManager
+    androidNotificationManager.createNotificationChannel(channel)
+    notificationManager = androidNotificationManager
+  }
+
+  // TODO: update token in RN
+  override fun onNewToken(token: String) {
+    Log.i(FIREBASE_TAG, "new token received: $token")
+    super.onNewToken(token)
+  }
+
+  @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
+  override fun onMessageReceived(message: RemoteMessage) {
+    Log.i(FIREBASE_TAG, "message received")
+
+    val notificationData = NotificationData(message.data)
+    val notification = createNotification(notificationData)
+
+    notificationManager?.let {
+      Log.i(FIREBASE_TAG, "message displayed")
+      notificationManager!!.notify(NOTIFICATION_ID, notification)
+    }
+  }
+
+  private fun createNotification(notificationData: NotificationData): Notification {
+    val notificationBuilder =
+      NotificationCompat.Builder(this, CHANNEL_ID)
+        .setContentTitle(notificationData.title)
+        .setSmallIcon(android.R.drawable.star_on)
+        .setContentText(notificationData.subtitle)
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA) {
+      notificationBuilder.setShortCriticalText("SWM")
+      notificationBuilder.setOngoing(true)
+      notificationBuilder.setRequestPromotedOngoing(true)
+
+      val progressStyle = createProgressStyle(notificationData)
+      notificationBuilder.setStyle(progressStyle)
     }
 
-    // TODO: update token in RN
-    override fun onNewToken(token: String) {
-        Log.i(FIREBASE_TAG, "new token received: $token")
-        super.onNewToken(token)
+    return notificationBuilder.build()
+  }
+
+  private fun createProgressStyle(
+    notificationData: NotificationData
+  ): NotificationCompat.ProgressStyle {
+    val progressStyle = NotificationCompat.ProgressStyle()
+
+    notificationData.progress?.let { progressStyle.setProgress(it) }
+
+    notificationData.progressPoints.forEach { it ->
+      progressStyle.addProgressPoint(NotificationCompat.ProgressStyle.Point(it))
     }
 
-    @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
-    override fun onMessageReceived(message: RemoteMessage) {
-        Log.i(FIREBASE_TAG, "message received")
-
-        val notificationData = NotificationData(message.data)
-        val notification = createNotification(notificationData)
-
-        notificationManager?.let {
-            Log.i(FIREBASE_TAG, "message displayed")
-            notificationManager!!.notify(NOTIFICATION_ID, notification)
-        }
-    }
-
-    private fun createNotification(
-        notificationData: NotificationData
-    ): Notification {
-        val notificationBuilder =
-            NotificationCompat
-                .Builder(
-                    this,
-                    CHANNEL_ID,
-                )
-                .setContentTitle(notificationData.title)
-                .setSmallIcon(android.R.drawable.star_on)
-                .setContentText(notificationData.subtitle)
-
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.BAKLAVA){
-            notificationBuilder.setShortCriticalText("SWM")
-            notificationBuilder.setOngoing(true)
-            notificationBuilder.setRequestPromotedOngoing(true)
-
-            val progressStyle = createProgressStyle(notificationData)
-            notificationBuilder.setStyle(progressStyle)
-        }
-
-        return notificationBuilder.build()
-    }
-
-    private fun createProgressStyle(notificationData: NotificationData): NotificationCompat.ProgressStyle {
-        val progressStyle = NotificationCompat.ProgressStyle()
-
-        notificationData.progress?.let {
-            progressStyle.setProgress(it)
-        }
-
-        notificationData.progressPoints.forEach { it -> progressStyle.addProgressPoint(
-            NotificationCompat.ProgressStyle.Point(it))}
-
-        return progressStyle
-    }
+    return progressStyle
+  }
 }
