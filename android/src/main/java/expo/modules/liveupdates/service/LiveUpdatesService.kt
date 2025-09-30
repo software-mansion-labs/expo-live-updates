e9fc81be89dfaca21e44face04d64cefefbe0c7f
AnCichocka <annacichocka.poczta@gmail.com>
2025-09-30 14:42:18 +0200

    fix formatting

--- android/src/main/java/expo/modules/liveupdates/service/LiveUpdatesService.kt
+++ android/src/main/java/expo/modules/liveupdates/service/LiveUpdatesService.kt
@@ -2,10 +2,8 @@ package expo.modules.liveupdates
 
 import android.Manifest
 import android.app.Notification
-import android.app.PendingIntent
 import android.app.Service
 import android.content.BroadcastReceiver
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
