a22059b1735dd6c60d25b420e3beea719ae2588f
AnCichocka <annacichocka.poczta@gmail.com>
2025-10-09 11:52:58 +0200

    fix formatting

--- android/src/main/java/expo/modules/liveupdates/service/LiveUpdatesEvents.kt
+++ android/src/main/java/expo/modules/liveupdates/service/LiveUpdatesEvents.kt
@@ -1,6 +1,6 @@
 package expo.modules.liveupdates.service
 
 object LiveUpdatesEvents {
-    const val onTokenChange = "onTokenChange"
-    const val onNotificationStateChange = "onNotificationStateChange"
+  const val onTokenChange = "onTokenChange"
+  const val onNotificationStateChange = "onNotificationStateChange"
 }
--- android/src/main/java/expo/modules/liveupdates/service/NotificationDismissedReceiver.kt
+++ android/src/main/java/expo/modules/liveupdates/service/NotificationDismissedReceiver.kt
@@ -6,7 +6,9 @@ import android.content.Intent
 import android.util.Log
 import expo.modules.liveupdates.NotificationAction
 import expo.modules.liveupdates.NotificationStateEventEmitter
+
 const val TAG = "NotificationDismissedReceiver"
+
 class NotificationDismissedReceiver : BroadcastReceiver() {
 
   override fun onReceive(context: Context, intent: Intent) {
