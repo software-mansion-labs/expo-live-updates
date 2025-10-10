a670467e53fbf169db490ff5cee2639486c3ef9e
Jan Ciesielczyk <jan.ciesielczyk@swmansion.com>
2025-10-10 11:30:38 +0200

    merge

--- android/src/main/java/expo/modules/liveupdates/FirebaseService.kt
+++ android/src/main/java/expo/modules/liveupdates/FirebaseService.kt
@@ -9,24 +9,21 @@ import androidx.annotation.RequiresPermission
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import expo.modules.liveupdates.service.LiveUpdatesManager
+import expo.modules.liveupdates.service.TokenChangeHandler
 
 const val FIREBASE_TAG = "FIREBASE SERVICE"
 
 class FirebaseService : FirebaseMessagingService() {
 
-  var notificationManager: NotificationManager? = null
   private var liveUpdatesManager: LiveUpdatesManager? = null
+  val tokenChangeHandler: TokenChangeHandler = TokenChangeHandler()
 
   @RequiresApi(Build.VERSION_CODES.O)
   override fun onCreate() {
     liveUpdatesManager = LiveUpdatesManager(this, CHANNEL_ID)
   }
 
-  // TODO: update token in RN
-  override fun onNewToken(token: String) {
-    Log.i(FIREBASE_TAG, "new token received: $token")
-    super.onNewToken(token)
-  }
+  override fun onNewToken(token: String) = tokenChangeHandler.onNewToken(token)
 
   @RequiresPermission(Manifest.permission.POST_NOTIFICATIONS)
   override fun onMessageReceived(message: RemoteMessage) {
