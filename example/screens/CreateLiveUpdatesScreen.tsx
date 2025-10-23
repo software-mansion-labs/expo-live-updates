50e21f33e5072d99969885e0ae23ca19e0f4c8a9
AnCichocka <annacichocka.poczta@gmail.com>
2025-10-23 14:39:36 +0200

    clean up after merge

--- example/screens/CreateLiveUpdatesScreen.tsx
+++ example/screens/CreateLiveUpdatesScreen.tsx
@@ -27,7 +27,6 @@ import * as Clipboard from 'expo-clipboard'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { Asset } from 'expo-asset'
 import IOSPlaceholder from './IOSPlaceholder'
-import { Asset } from 'expo-asset'
 
 const toggle = (previousState: boolean) => !previousState
 
